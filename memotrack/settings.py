1884ff53ffee2b0bfdc9d38f6062b1a79a8beaa5
Rianne Celone Tudtud <rtudtud@ssct.edu.ph>
2026-05-09 00:51:21 +0800

    Use unsigned Cloudinary uploads with 'skedit' preset

--- memotrack/settings.py
+++ memotrack/settings.py
@@ -152,6 +152,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'UPLOAD_PRESET': 'skedit',
 }
 
 if CLOUDINARY_STORAGE['CLOUD_NAME']:
