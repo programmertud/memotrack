1e3b86bcb2140c4287e9af0ec07d86ba8a28a177
Rianne Celone Tudtud <rtudtud@ssct.edu.ph>
2026-05-09 20:38:55 +0800

    Fix ModuleNotFoundError for public app and update runtime to 3.12

--- memotrack/wsgi.py
+++ memotrack/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add the project root directory to the sys.path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memotrack.settings')
 
